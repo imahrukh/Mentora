22770f5adb1c21bdec7f95c32500ee189531c133
Mah Rukh <me.mahrukhch@gmail.com>
2025-04-29 20:11:54 +0500

    imported packages to main activity

--- app/src/main/java/com/fast/mentor/MainActivity.java
+++ app/src/main/java/com/fast/mentor/MainActivity.java
@@ -1,12 +1,12 @@
 package com.fast.mentor;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
