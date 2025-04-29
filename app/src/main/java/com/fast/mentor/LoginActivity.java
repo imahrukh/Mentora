235732ec4f08c592f2eefb54d54fede266c22a1b
Mah Rukh <me.mahrukhch@gmail.com>
2025-04-29 20:54:35 +0500

    LoginActivity that takes email and password as input

---
+++ app/src/main/java/com/fast/mentor/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.fast.mentor;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailInput, passwordInput;
+    private Button loginButton;
+    private TextView signupRedirect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailInput = findViewById(R.id.emailInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        loginButton = findViewById(R.id.loginButton);
+        signupRedirect = findViewById(R.id.signupRedirect);
+
+        // Login click (you can add Firebase or logic later)
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: validate login here
+            }
+        });
+
+        // Redirect to Sign Up
+        signupRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
